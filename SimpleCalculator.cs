75e2c9d3e052241d2d842199dda58c39736cabdf
Vladyslav Mykhaylyuk <vladyslaff87@gmail.com>
2018-02-19 23:16:09 +0200

    Added SimpleCalculator and UserPassword classes implementation

---
+++ SimpleCalculator.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace TDD.SimpleCalculator
+{
+    public class SimpleCalculator
+    {
+        private string _delimitersPrefix = "//";
+
+        private bool CanAddNumbers(string numbers)
+        {
+            return !string.IsNullOrEmpty(numbers);
+        }
+
+        public int Add(string numbers)
+        {
+            if (!CanAddNumbers(numbers))
+            {
+                return 0;
+            }
+            
+            var providedStringType = CalculatorIsOfType(numbers);
+            List<string> delimiters = ParseDelimiters(providedStringType, numbers);
+            var parsedNumbers = ParseNumbers(numbers, delimiters, providedStringType);
+            
+            return Add(parsedNumbers);
+        }
+
+        private int Add(string[] parsedNumbers)
+        {
+            var sum = 0;
+            var negativeNumbers = new List<int>();
+
+            foreach (var parsedNumber in parsedNumbers)
+            {
+                var numberToAdd = int.Parse(parsedNumber.Trim());
+                if (numberToAdd < 0)
+                {
+                    negativeNumbers.Add(numberToAdd);
+                }
+                else if (numberToAdd <= 1000)
+                {
+                    sum += numberToAdd;
+                }
+            }
+
+            if (negativeNumbers.Count > 0)
+            {
+                throw new Exception($"Negatives are not allowed: {string.Join(", ", negativeNumbers)}");
+            }
+
+            return sum;
+        }
+
+        private string[] ParseNumbers(string numbersAndDelimiters, List<string> delimiters, StringType providedStringType)
+        {
+            var numbersString = providedStringType != StringType.NoDelimitersString ? numbersAndDelimiters.Substring(numbersAndDelimiters.IndexOf("\n") + 1) : numbersAndDelimiters;
+            return numbersString.Split(delimiters.ToArray(), StringSplitOptions.None);
+        }
+
+        private List<string> ParseDelimiters(StringType providedStringType, string numbersAndDelimiters)
+        {
+            switch (providedStringType)
+            {
+                case StringType.ComplexDelimiter:
+                    return ParseComplexDelimiters(numbersAndDelimiters);
+                case StringType.OneCharDelimiter:
+                    return ParseOneCharDelimiter(numbersAndDelimiters);
+                case StringType.NoDelimitersString:
+                    return DefaultDelimiters();
+                default:
+                    return new List<string>();
+            }
+        }
+
+        private List<string> ParseComplexDelimiters(string numbersAndDelimiters)
+        {
+            var delimitersList = new List<string>();
+            var r = new Regex(@"\[([^]]*)\]");
+            Match m = r.Match(numbersAndDelimiters);
+            while (m.Success)
+            {
+                delimitersList.Add(m.Groups[1].Value);
+                m = m.NextMatch();
+            }
+
+            return delimitersList;
+        }
+
+        private List<string> ParseOneCharDelimiter(string numbersAndDelimiters)
+        {
+            int delimiterIndex = numbersAndDelimiters.IndexOf(_delimitersPrefix) + 2;
+            return new List<string>
+            {
+                numbersAndDelimiters.Substring(delimiterIndex, numbersAndDelimiters.IndexOf("\n") - delimiterIndex)
+            };
+        }
+
+        private List<string> DefaultDelimiters()
+        {
+            return new List<string>() { ",", "\n" };
+        }
+
+        private StringType CalculatorIsOfType(string numbers)
+        {
+            var providedStringType = StringType.NoDelimitersString;
+            if (numbers.StartsWith(_delimitersPrefix))
+            {
+                providedStringType = IsDelimetersListInBrackets(numbers) ? StringType.ComplexDelimiter : StringType.OneCharDelimiter;
+            }
+            return providedStringType;
+        }
+
+        private bool IsDelimetersListInBrackets(string delimetersAndNumbers)
+        {
+            var r = new Regex(@"^(\/\/)(\[([^]]*)\])*\n.+");
+            return r.IsMatch(delimetersAndNumbers);
+        }
+
+        private enum StringType
+        {
+            NoDelimitersString,
+            OneCharDelimiter,
+            ComplexDelimiter
+        }
+    }
+}
---
+++ UserPassword.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TDD.Password
+{
+    public class UserPassword
+    {
+        public string Password { get; set; }
+
+        public bool IsValid
+        {
+            get
+            {
+                return !string.IsNullOrEmpty(Password) && Regex.IsMatch(Password, "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\da-zA-Z]).{5,10}$");
+            }
+        }
+
+        public int Length
+        {
+            get
+            {
+                return Password.Length;
+            }
+        }
+
+        public UserPassword(string password)
+        {
+            Password = password;
+        }
+
+        public UserPassword()
+        {
+            var rnd = new Random();
+            var strLength = rnd.Next(5, 10);
+            Password = GeneratePassword(strLength);
+        }
+
+        private string GeneratePassword(int length)
+        {
+            var complexity = 10;
+            System.Security.Cryptography.RNGCryptoServiceProvider csp =
+                new System.Security.Cryptography.RNGCryptoServiceProvider();
+            char[][] classes =
+            {
+                @"abcdefghijklmnopqrstuvwxyz".ToCharArray(),
+                @"ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray(),
+                @"0123456789".ToCharArray(),
+                @" !""#$%&'()*+,./:;<>?@[\]^_{|}~".ToCharArray(),
+            };
+
+            complexity = Math.Max(1, Math.Min(classes.Length, complexity));
+
+            char[] allchars = classes.Take(complexity).SelectMany(c => c).ToArray();
+            byte[] bytes = new byte[allchars.Length];
+            csp.GetBytes(bytes);
+            for (int i = 0; i < allchars.Length; i++)
+            {
+                char tmp = allchars[i];
+                allchars[i] = allchars[bytes[i] % allchars.Length];
+                allchars[bytes[i] % allchars.Length] = tmp;
+            }
+
+            Array.Resize(ref bytes, length);
+            char[] result = new char[length];
+
+            while (true)
+            {
+                csp.GetBytes(bytes);
+
+                for (int i = 0; i < length; i++)
+                {
+                    result[i] = allchars[bytes[i] % allchars.Length];
+                }
+
+                if (Char.IsWhiteSpace(result[0]) || Char.IsWhiteSpace(result[(length - 1) % length]))
+                {
+                    continue;
+                }
+                    
+                string testResult = new string(result);
+
+                if (0 != classes.Take(complexity).Count(c => testResult.IndexOfAny(c) < 0))
+                {
+                    continue;
+                }   
+
+                return testResult;
+            }
+        }
+    }
+}
